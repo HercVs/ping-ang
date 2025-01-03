0592bf1f4c1cc5d4c4487a9418f7576e5aabc41b
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-03 08:24:03 +0200

    announcement & announc holders components

---
+++ src/app/components/news-department/news-department.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-department',
+  imports: [],
+  templateUrl: './news-department.component.html',
+  styleUrl: './news-department.component.css'
+})
+export class NewsDepartmentComponent {
+
+}
---
+++ src/app/components/news-general/news-general.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-general',
+  imports: [],
+  templateUrl: './news-general.component.html',
+  styleUrl: './news-general.component.css'
+})
+export class NewsGeneralComponent {
+
+}
