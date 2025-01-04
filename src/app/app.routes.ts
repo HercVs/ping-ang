77f00506ff548604656f9c788f060007079a1fb9
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-04 13:08:48 +0200

    main component & routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { authGuard } from './shared/guards/auth.guard';
+import { NewsGeneralComponent } from './components/news-general/news-general.component';
+import { NewsDepartmentComponent } from './components/news-department/news-department.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: LandingPageComponent,
+	},
+	{
+		path: 'login',
+		component: UserLoginComponent,
+	},
+	{
+		path: 'home',
+		component: MainPageComponent,
+		canActivate: [authGuard],
+		children: [
+			{
+				path: '',
+				component: NewsGeneralComponent,
+			},
+			{
+				path: ':departmentId', // TODO name instead ?
+				component: NewsDepartmentComponent,
+			},
+		],
+	},
+];
