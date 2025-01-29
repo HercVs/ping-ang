2e1f3e6186963d6cffa383bfc5c86edde3bc49fe
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-29 16:00:47 +0200

    sidebar functionality

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MenuItem } from '../../shared/interfaces/menu-item';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-sidebar',
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+	selector: 'app-sidebar',
+	imports: [RouterLink],
+	templateUrl: './sidebar.component.html',
+	styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-
+	@Input() items: MenuItem[] | undefined;
 }
