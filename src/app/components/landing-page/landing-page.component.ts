b6ccea8a7ec554d7355c887ff64a518798a3f5a0
herc <101748155+mouxmoux@users.noreply.github.com>
2025-01-31 10:04:00 +0200

    minor

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../shared/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-landing-page',
-  imports: [],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+	selector: 'app-landing-page',
+	imports: [],
+	templateUrl: './landing-page.component.html',
+	styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
-
+	router: Router = inject(Router);
+	userService: UserService = inject(UserService);
 }
