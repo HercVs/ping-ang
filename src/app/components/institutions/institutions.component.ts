f339c795dbcf02a54553d4d3d15f5cc57d253539
herc <101748155+mouxmoux@users.noreply.github.com>
2025-02-03 06:52:26 +0200

    dynamic department menu

--- src/app/components/institutions/institutions.component.ts
+++ src/app/components/institutions/institutions.component.ts
@@ -5,7 +5,10 @@ import {
 	Department,
 	Institution,
 	School,
+	SubscriptionDetails,
 } from '../../shared/interfaces/institutions';
+import { UserService } from '../../shared/services/user.service';
+import { byProperty } from '../../shared/utils/util';
 
 @Component({
 	selector: 'app-institutions',
@@ -14,6 +17,8 @@ import {
 	styleUrl: './institutions.component.css',
 })
 export class InstitutionsComponent {
+	// TODO demo functionality
+	userService = inject(UserService);
 	institutionService = inject(InstitutionService);
 
 	countries: Country[] = [];
@@ -25,6 +30,8 @@ export class InstitutionsComponent {
 	selectedInstitution: number = 0;
 	selectedSchool: number = 0;
 	selectedDepartment: number = 0;
+
+	departmentsError: string | null = null;
 
 	ngOnInit() {
 		this.resetSelectors();
@@ -41,36 +48,101 @@ export class InstitutionsComponent {
 		this.selectedSchool = 0;
 		this.selectedDepartment = 0;
 
-		this.institutionService.getAllCountries().subscribe((data) => {
-			this.countries = data;
+		this.departmentsError = null;
+
+		this.institutionService.getAllCountries().subscribe({
+			next: (data) => {
+				this.countries = data;
+			},
+			error: (err) => {},
 		});
 	}
 
 	findInstitutions(country: number) {
-		this.institutionService.getInstitutions(country).subscribe((data) => {
-			this.institutions = data;
-			this.selectedCountry = country;
-			console.log(data);
+		this.departmentsError = null;
+		this.institutionService.getInstitutions(country).subscribe({
+			next: (data) => {
+				this.institutions = data.sort(byProperty('name'));
+			},
+			error: (err) => {
+				console.log('error');
+			},
 		});
+		this.selectedCountry = country;
+		this.selectedInstitution = 0;
+		this.selectedSchool = 0;
+		this.selectedDepartment = 0;
+
+		this.schools = [];
+		this.departments = [];
+
+		this.departmentsError = null;
 	}
 
 	findSchools(institutionId: number) {
-		this.institutionService.getSchools(institutionId).subscribe((data) => {
-			this.schools = data;
-			this.selectedInstitution = institutionId;
-			console.log(data);
+		this.departmentsError = null;
+		this.institutionService.getSchools(institutionId).subscribe({
+			next: (data) => {
+				this.schools = data.sort(byProperty('school'));
+			},
+			error: (err) => {
+				console.log('error');
+			},
 		});
+		this.selectedInstitution = institutionId;
+		this.selectedSchool = 0;
+		this.selectedDepartment = 0;
+
+		this.departments = [];
 	}
 
 	findDepartments(institutionId: number, schoolId: number) {
+		this.departmentsError = null;
+		this.institutionService.getDepartments(institutionId, schoolId).subscribe({
+			next: (data) => {
+				this.departments = data.sort(byProperty('department'));
+			},
+			error: (err) => {
+				if (err.error.code === 'DepartmentNotFound') {
+					this.departments = [];
+					this.departmentsError = 'No departments found for school';
+				}
+			},
+		});
 		this.selectedInstitution = institutionId;
-		this.institutionService
-			.getDepartments(institutionId, schoolId)
-			.subscribe((data) => {
-				this.departments = data;
-				console.log(data);
-			});
+		this.selectedSchool = schoolId;
+		this.selectedDepartment = 0;
 	}
 
-	subscribeNewsletter(departmentId: number) {}
+	subscribeToNewsletter(departmentId: number) {
+		const subscription: SubscriptionDetails = {
+			userId: this.userService.user()?.id as unknown as number,
+			departmentId: departmentId,
+		};
+		this.institutionService.subscribeToDepartment(subscription).subscribe({
+			next: (data) => {
+				console.log(data);
+				this.institutionService.refreshSubscriptions();
+			},
+			error: (err) => {
+				console.log(err);
+			},
+		});
+	}
+
+	unsubscribeFromNewsletter(departmentId: number) {
+		const subscription: SubscriptionDetails = {
+			userId: this.userService.user()?.id as unknown as number,
+			departmentId: departmentId,
+		};
+		this.institutionService.unsubscribeFromDepartment(subscription).subscribe({
+			next: (data) => {
+				console.log(data);
+				this.institutionService.refreshSubscriptions(); // TODO doesn't refresh like subscribing does
+			},
+			error: (err) => {
+				console.log(err);
+			},
+		});
+	}
 }
