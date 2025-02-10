8ea77f11a5c6a61602bcba75a8a7037dd85cb350
herc <101748155+mouxmoux@users.noreply.github.com>
2025-02-10 15:35:55 +0200

    sleep util

--- src/app/shared/utils/util.ts
+++ src/app/shared/utils/util.ts
@@ -12,3 +12,7 @@ export function byProperty<T>(key: keyof T) {
 		return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
 	};
 }
+
+export function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
